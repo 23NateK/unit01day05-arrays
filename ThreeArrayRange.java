9f8ef5887740b98b30bd25eb1b543eda19fb31a4
23NateK <n.kimber22@gmail.com>
2023-01-19 12:29:17 -0600

    Finished ThreeArrayRange

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
 public class ThreeArrayRange {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        Arrays.toString(a);
+        if (a[0]<a[1]&&a[0]<a[2]){
+            if (a[1]>a[2]){
+                System.out.println(a[1]-a[0]);
+            }else{
+                System.out.println(a[2]-a[0]);
+            }
+        }else if (a[1]<a[0]&&a[1]<a[2]){
+            if (a[0]>a[2]){
+                System.out.println(a[0]-a[1]);
+            }else{
+                System.out.println(a[2]-a[1]);
+            }
+        }else{
+            if (a[1]>a[0]){
+                System.out.println(a[1]-a[2]);
+            }else{
+                System.out.println(a[0]-a[2]);
+            }
+        }
     }
 }
