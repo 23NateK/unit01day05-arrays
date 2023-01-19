9f26358aae535e8423071519bcef947eae675962
23NateK <n.kimber22@gmail.com>
2023-01-19 12:21:31 -0600

    Finished ThreeArrayProduct

--- ThreeArrayProduct.java
+++ ThreeArrayProduct.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
 public class ThreeArrayProduct {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
-        
+        Arrays.toString(a);
+        int result = a[0]+a[1]+a[2];
+        System.out.println(result);
     }
 }
