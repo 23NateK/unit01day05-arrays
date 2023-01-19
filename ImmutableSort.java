e725cd5c83d45a32cf158a004e3062afa83a7c85
23NateK <n.kimber22@gmail.com>
2023-01-19 12:06:06 -0600

    Finished ImmutableSort

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,38 @@
-public class ImmutableSort {
+
+        import java.util.Arrays;
+        public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
-        
+
+        int[] a = {4, 0, 3};
+        int[] b = {0, 0, 0};
+        if (a[0] < a[1] && a[0] < a[2]){
+            b[0] = a[0];
+            if (a[1]<a[2]){
+                b[1] = a[1];
+                b[2] = a[2];
+            }else{
+                b[1] = a[2];
+                b[2] = a[1];
+            }
+        }else if((a[0]<a[1] && a[0]>a[2]) || (a[0]<a[2]&&a[0]>a[1])){
+            b[1] = a[0];
+            if (a[1]<a[2]){
+                b[0] = a[1];
+                b[2] = a[2];
+            }else{
+                b[0] = a[2];
+                b[2] = a[1];
+            }
+        }else{
+            b[2] = a[0];
+            if (a[1]<a[2]){
+                b[0] = a[1];
+                b[1] = a[2];
+            }else{
+                b[0] = a[2];
+                b[1] = a[1];
+            }
+        }
+        System.out.println(Arrays.toString(a)+" | "+ Arrays.toString(b));
     }
 }
