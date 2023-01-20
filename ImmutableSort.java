ca2a1b660a052a64e1bdc702d53aa53f1615613d
23NateK <n.kimber22@gmail.com>
2023-01-20 11:54:41 -0600

    fixed ImmutableSort

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -3,9 +3,41 @@
         public class ImmutableSort {
     public static void main(String[] args) {
 
-        int[] a = {4, 0, 3};
+        int[] a = {0, 0, 3};
         int[] b = {0, 0, 0};
-        if (a[0] < a[1] && a[0] < a[2]){
+        if (a[0]==a[1]||a[1]==a[2]||a[0]==a[2]){
+            if (a[0]==a[1]){
+                if (a[0]>a[2]){
+                    b[0]=a[2];
+                    b[1]=a[1];
+                    b[2]=a[0];
+                }else{
+                    b[0]=a[0];
+                    b[1]=a[1];
+                    b[2]=a[2];
+                }
+            }else if(a[1]==a[2]){
+                if (a[0]>a[1]){
+                    b[0]=a[2];
+                    b[1]=a[1];
+                    b[2]=a[0];
+                }else{
+                    b[0]=a[0];
+                    b[1]=a[1];
+                    b[2]=a[2];
+                }
+            }else{
+                if (a[1]>a[2]){
+                    b[0]=a[2];
+                    b[1]=a[2];
+                    b[2]=a[1];
+                }else{
+                    b[0]=a[1];
+                    b[1]=a[0];
+                    b[2]=a[2];
+                }
+            }
+        }else if (a[0] < a[1] && a[0] < a[2]){
             b[0] = a[0];
             if (a[1]<a[2]){
                 b[1] = a[1];
